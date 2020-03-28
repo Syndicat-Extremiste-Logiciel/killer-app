7359e0338922c038b6ddda3e3f30c07aadf44eae
Rodrigue Couanon <rodrigue@exmachina.nl>
2020-03-28 21:41:57 +0100

    Adds styles

--- src/app.tsx
+++ src/app.tsx
@@ -3,4 +3,6 @@ import * as ReactDOM from 'react-dom';
 
 import Greet from './components/greet';
 
+// TODO - Create APP loader with Socket Connection + Redux + Router
+
 ReactDOM.render(<Greet />, document.getElementById('app'));
--- src/components/greet.tsx
+++ src/components/greet.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 
 export default class Greet extends React.Component {
   render() {
-    return (<div>
-      <h1> SUCE MOI NATHAN </h1>
-    </div>);
+    return (
+      <div className="test">
+        <h1> SUCE MOI NATHAN </h1>
+      </div>
+    );
   }
 }
